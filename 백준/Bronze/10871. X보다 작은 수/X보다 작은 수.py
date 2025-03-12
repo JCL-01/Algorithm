71f74a978eef0b2aac1ad843e237b346aa32bc2c
JCL-01 <129758734+JCL-01@users.noreply.github.com>
2025-03-12 18:24:23 +0900

    [Bronze V] Title: X보다 작은 수, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,6 +1,6 @@
 import sys
-N, X = map(int, sys.stdin.readline().rstrip().split())
-num_arr = sys.stdin.readline().rstrip().split()
+N, X = map(int, sys.stdin.readline().split())
+num_arr = sys.stdin.readline().split()
 small_num_list = []
 for i in range(N):
     if int(num_arr[i]) >= X:
