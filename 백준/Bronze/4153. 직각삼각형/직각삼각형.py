9ab6630d437ead36ecabf587b94dd115773a6baa
JCL-01 <129758734+JCL-01@users.noreply.github.com>
2025-03-12 23:50:35 +0900

    [Bronze III] Title: 직각삼각형, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.py
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -2,14 +2,14 @@ import sys
 while True:
     try:
         a, b, c = map(int, sys.stdin.readline().split())
-        len_list = [a, b, c]
-        len_list.sort()
     except:
         break
     else:
-        if (a, b, c) == (0, 0, 0):
+        len_list = [a, b, c]
+        len_list.sort()
+        if len_list[0] == 0:
             break
         if len_list[2]**2 == len_list[0]**2 + len_list[1]**2:
-            sys.stdout.write("right"+'\n')
+            sys.stdout.write("right\n")
         else:
-            sys.stdout.write("wrong"+'\n')
+            sys.stdout.write("wrong\n")
