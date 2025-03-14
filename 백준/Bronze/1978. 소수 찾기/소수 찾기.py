679f560dae2130725abbe5adfb55e1c12c3ee023
JCL-01 <129758734+JCL-01@users.noreply.github.com>
2025-03-14 09:23:55 +0900

    [Bronze II] Title: 소수 찾기, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,13 +1,21 @@
 import sys
-sys.stdin.readline()
-decimal_list = list(range(2, 1001))
+import math
+_ = sys.stdin.readline()
+num_list = list(map(int, sys.stdin.readline().split()))
 decimal_num = 0
-for i in range(2, 1001):
-    for j in range(i+1, 1001):
-        if j in decimal_list:
-            if j % i == 0:
-                decimal_list.remove(j)
-for num in map(int, sys.stdin.readline().split()):
-    if num in decimal_list:
+is_decimal = True
+for i in num_list:
+    if i == 1:
+        continue
+    if i == 2 or i == 3:
         decimal_num += 1
+        continue
+    for j in range(2, int(math.sqrt(i)) + 1):
+        if i % j == 0:
+            is_decimal = False
+            break
+    if is_decimal == True:
+        decimal_num += 1
+    is_decimal = True
+    
 sys.stdout.write(str(decimal_num))
