471f081d8488626ca02ead5b318781fe09b56d2d
JCL-01 <129758734+JCL-01@users.noreply.github.com>
2025-03-20 18:21:42 +0900

    [Bronze II] Title: 분해합, Time: 1352 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,24 +1,12 @@
 import sys
 N = int(sys.stdin.readline())
-constructor_list = []
-for i in range(1, N):
-    digit_num = 1
-    tmp = i
-    while True:
-        if tmp / 10 >= 1:
-            tmp /= 10
-            digit_num += 1
-            continue
+for i in range(1, N+1):
+    if i == N:
+        sys.stdout.write('0')
         break
     digit_sum = 0
-    tmp = i
-    for _ in range(digit_num):
-        digit_sum += tmp % 10
-        tmp //= 10
+    for digit in map(int, str(i)):
+        digit_sum += digit
     if i + digit_sum == N:
-        constructor_list.append(i)
-constructor_list.sort()
-if constructor_list == []:
-    sys.stdout.write('0')
-else:
-    sys.stdout.write(str(constructor_list[0]))
+        sys.stdout.write(str(i))
+        break
