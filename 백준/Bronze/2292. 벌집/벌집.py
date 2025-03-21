a17468432214015f9c84565e8274e0ed1cb939b1
JCL-01 <129758734+JCL-01@users.noreply.github.com>
2025-03-21 17:06:46 +0900

    [Bronze II] Title: 벌집, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -3,6 +3,6 @@ N = int(sys.stdin.readline())
 room_num = 1
 for i in range(1, 1000000000):
     if N <= room_num:
-        sys.stdout.write(str(i))
+        sys.stdout.write(f'{i}')
         break
     room_num += 6 * i
