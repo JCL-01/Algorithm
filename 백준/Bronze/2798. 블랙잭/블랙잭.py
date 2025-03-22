193d17f3730e5b4d76091ad804fec68d15375aa8
JCL-01 <129758734+JCL-01@users.noreply.github.com>
2025-03-22 22:16:43 +0900

    [Bronze II] Title: 블랙잭, Time: 112 ms, Memory: 39296 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,16 @@
+import sys
+N, M, *num_list = map(int, sys.stdin.read().split())
+num_list.sort()
+sum_list = []
+for i in range(N):
+    if num_list[i] * 3 > M:
+        continue
+    for j in range(i + 1, N):
+        if num_list[i] + num_list[j] * 2 > M:
+            continue
+        for k in range(j + 1, N):
+            if num_list[i] + num_list[j] + num_list[k] > M:
+                continue
+            sum_list.append(num_list[i] + num_list[j] + num_list[k])
+sum_list.sort()
+sys.stdout.write(f'{sum_list[-1]}')
