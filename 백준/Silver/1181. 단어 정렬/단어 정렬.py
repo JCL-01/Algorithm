c7853d8a35e7fb6275b2118e1ce18331ea2fb409
JCL-01 <129758734+JCL-01@users.noreply.github.com>
2025-03-24 19:24:49 +0900

    [Silver V] Title: 단어 정렬, Time: 64 ms, Memory: 39508 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -3,5 +3,5 @@ N = int(sys.stdin.readline())
 word_set = set()
 for word in sys.stdin.read().split():
     word_set.add(word)
-result = sorted(word_set, key=lambda x:(len(x), x))
-sys.stdout.write('\n'.join(result))
+word_list = sorted(word_set, key=lambda x:(len(x), x))
+sys.stdout.write('\n'.join(word_list))
