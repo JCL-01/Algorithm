e45ff9269967b0fb51c330007c6ae3ab3a82f1a1
JCL-01 <129758734+JCL-01@users.noreply.github.com>
2025-05-08 09:32:39 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 64 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,26 +1,21 @@
 import sys
 N, M, *lines = sys.stdin.read().split()
 repaint_num_list = []
-color_opt_list = [('W', 'B'), ('B', 'W')]
-for color_opt in color_opt_list:
-    for start_row in range(int(N) - 7):
-        for start_column in range(int(M) - 7):
-            repaint_num = 0
-            for row in range(start_row, start_row + 8):
-                for column in range(start_column, start_column + 8):
-                    if row % 2 == 0:
-                        if column % 2 == 0:
-                            if lines[row][column] != color_opt[0]:
-                                repaint_num += 1
-                        else:
-                            if lines[row][column] != color_opt[1]:
-                                repaint_num += 1
+for start_row in range(int(N) - 7):
+    for start_column in range(int(M) - 7):
+        W_repaint_num = 0
+        B_repaint_num = 0
+        for row in range(start_row, start_row + 8):
+            for column in range(start_column, start_column + 8):
+                if (row + column) % 2 == 0:
+                    if lines[row][column] != 'W':
+                        W_repaint_num += 1
                     else:
-                        if column % 2 != 0:
-                            if lines[row][column] != color_opt[0]:
-                                repaint_num += 1
-                        else:
-                            if lines[row][column] != color_opt[1]:
-                                repaint_num += 1
-            repaint_num_list.append(repaint_num)
+                        B_repaint_num += 1
+                else:
+                    if lines[row][column] == 'W':
+                        W_repaint_num += 1
+                    else:
+                        B_repaint_num += 1
+        repaint_num_list.extend([W_repaint_num, B_repaint_num])
 sys.stdout.write(f'{min(repaint_num_list)}')
